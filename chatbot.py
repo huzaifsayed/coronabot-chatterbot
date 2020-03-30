ffabffa8184c3624d6532f01ab56244a865b5ff8
huzaifsayed <huzaif.sayed@gmail.com>
2020-03-30 20:53:05 +0530

    Advance Chat Bot

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,38 @@
 from chatterbot import ChatBot
 
-chatbot = ChatBot('CoronaBot')
+chatbot = ChatBot(
+    'CoronaBot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
 
+# Training With Own Questions 
 from chatterbot.trainers import ListTrainer
 
 trainer = ListTrainer(chatbot)
 
-training_data_quesans = open('training_data/quesans.txt').read().splitlines()
-training_data_greetings = open('training_data/greetings.txt').read().splitlines()
+training_data_quesans = open('training_data/ques_ans.txt').read().splitlines()
+training_data_personal = open('training_data/personal_ques.txt').read().splitlines()
 
-training_data = training_data_quesans + training_data_greetings
+training_data = training_data_quesans + training_data_personal
 
 trainer.train(training_data)
+
+# Training With Corpus
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+trainer_corpus = ChatterBotCorpusTrainer(chatbot)
+
+trainer_corpus.train(
+    'chatterbot.corpus.english'
+)
